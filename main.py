df3b69b50f1336b5758f3b60accab756be5bb2d4
tester-908 <alvar.sahlholm@gmail.com>
2026-09-12 17:31:04 +0200

    main.py wiht basic pygame

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1080, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
